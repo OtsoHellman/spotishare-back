94e972f21632dee972db5381b6306c813c720a50
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-05-10 23:04:53 +0300

    make hyytiala not expose his spotify credentials

--- src/app.js
+++ src/app.js
@@ -1,26 +1,42 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const SpotifyWebApi = require('spotify-web-api-node')
 
-require('dotenv').config();
+require('dotenv').config()
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require('./middlewares')
+const api = require('./api')
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(helmet());
+app.use(morgan('dev'))
+app.use(helmet())
+
+// credentials are optional
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENTID,
+  clientSecret: process.env.CLIENTSECRET,
+  redirectUri: process.env.REDIRECTURI
+})
+
+spotifyApi.setAccessToken(process.env.ACCESSTOKEN)
 
 app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
-  });
-});
+  // Get Elvis' albums
+  spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE').then(
+    (data) => {
+      res.json(data.body)
+    },
+    (err) => {
+      console.error(err)
+    }
+  )
+})
 
-app.use('/api/v1', api);
+app.use('/api/v1', api)
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+app.use(middlewares.notFound)
+app.use(middlewares.errorHandler)
 
-module.exports = app;
+module.exports = app
