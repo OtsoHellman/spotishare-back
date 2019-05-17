a0c67f2bf83721a3a1c7a92a6dcbd26d07bdb014
otsohellman <otso.hellman@gmail.com>
2019-05-17 21:05:00 +0300

    add logs

--- src/playback.js
+++ src/playback.js
@@ -7,7 +7,7 @@ exports.Playback = class Playback {
         this.spotifyApi = new SpotifyApi(accessToken, refreshToken)
         this.hash = hash
         this.startInterval()
-        
+
         this.spotifyApi.getUserInfo()
             .then(data => this.hostName = data.body.display_name)
             .catch(err => console.log(err))
@@ -31,6 +31,7 @@ exports.Playback = class Playback {
     }
 
     deactivateInterval = () => {
+        console.log("deactivating interval")
         clearInterval(this.playbackInterval)
         this.playbackInterval = false
     }
@@ -40,8 +41,9 @@ exports.Playback = class Playback {
             const progress = res.body.progress_ms
             const duration = res.body.item.duration_ms
             console.log(`Listening to ${res.body.item.name} on ${res.body.device.name}(${res.body.device.type}). Next song in ${parseInt((duration - progress) / 1000) - 3}s`)
-            console.log(`Songs still in queue: ${this.songQueue.map(song => "\n"+song.name)}`)
+            console.log(`Songs still in queue: ${this.songQueue.map(song => "\n" + song.name)}`)
             if (duration - progress < 3000 && this.songQueue.length > 0 && this.playbackInterval) {
+                console.log("Duration < 3s")
                 this.deactivateInterval()
                 this.playNextSong()
                     .then(this.startInterval())
@@ -50,6 +52,7 @@ exports.Playback = class Playback {
         .catch(err => console.log(err.message))
 
     startInterval = () => {
+        console.log("starting interval")
         this.playbackInterval = setInterval(() => this.pollPlayback(), 1000)
     }
 }
