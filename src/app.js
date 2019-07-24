9699b3aed59f35c9104d79f72aaab5c13df48380
Aapeli Haanpuu <aapeli.haanpuu@gofore.com>
2019-07-24 15:56:12 +0300

    Typo

--- src/app.js
+++ src/app.js
@@ -36,37 +36,34 @@ app.get('/login', (req, res) => {
     const redirectUri = req.query.redirectUri || config.redirectUri
     const scopes = 'user-modify-playback-state user-read-playback-state'
     const query = qs.stringify({
-        response_type: code,
+        response_type: 'code',
         client_id: config.clientId,
         scope: encodeURIComponent(scopes),
-        redirect_uri: encodeURIComponent(redirectUri)
+        redirect_uri: encodeURIComponent(redirectUri),
     })
     res.redirect(`https://accounts.spotify.com/authorize?${query}`)
 })
 
 app.get('/ok', (req, res) => {
-    const authorization = Buffer.from(
-        `${config.clientId}:${config.clientSecret}`,
-    ).toString('base64')
-    request.post(
-        {
-            uri: 'https://accounts.spotify.com/api/token',
-            form: {
-                code: req.query.code,
-                grant_type: 'authorization_code',
-                redirect_uri: config.redirectUri,
-            },
-            headers: {
-                Authorization: `Basic ${authorization}`,
-            },
+    const authorization = Buffer
+        .from(`${config.clientId}:${config.clientSecret}`)
+        .toString('base64')
+    request.post({
+        uri: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: req.query.code,
+            grant_type: 'authorization_code',
+            redirect_uri: config.redirectUri,
         },
-        (error, response, body) => {
-            const data = JSON.parse(body)
-            req.spotishare.access_token = data.access_token
-            req.spotishare.refresh_token = data.refresh_token
-            res.redirect(config.frontUri)
+        headers: {
+            Authorization: `Basic ${authorization}`,
         },
-    )
+    }, (error, response, body) => {
+        const data = JSON.parse(body)
+        req.spotishare.access_token = data.access_token
+        req.spotishare.refresh_token = data.refresh_token
+        res.redirect(config.frontUri)
+    })
 })
 app.use(middlewares.authentication)
 
