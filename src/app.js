3b0e5ba6d7bfd9b6c5b9fac3a2d40ca722b70f4e
Aapeli Haanpuu <aapeli.haanpuu@gofore.com>
2019-07-24 13:46:57 +0300

    Add query-string

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const session = require('./api/session')
 const config = require('./config')
 const request = require('request')
 const clientSession = require('client-sessions')
+const qs = require('query-string')
 cookieParser = require('cookie-parser')
 
 const app = express()
@@ -18,33 +19,34 @@ app.use(morgan('dev'))
 app.use(
     cors({
         origin: 'http://localhost:3000',
-        credentials: true
-    })
+        credentials: true,
+    }),
 )
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(
     clientSession({
         cookieName: 'spotishare',
-        secret: 'lihapulle'
-    })
+        secret: 'lihapulle',
+    }),
 )
 app.use(cookieParser())
 
 app.get('/login', (req, res) => {
     const redirectUri = req.query.redirectUri || config.redirectUri
-    const scopes = 'user-modify-playback-state user-read-playback-state';
-    res.redirect('https://accounts.spotify.com/authorize' +
-        '?response_type=code' +
-        '&client_id=' + config.clientId +
-        (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-        '&redirect_uri=' + encodeURIComponent(redirectUri)
-    )
+    const scopes = 'user-modify-playback-state user-read-playback-state'
+    const query = qs.stringify({
+        response_type: code,
+        client_id: config.clientId,
+        scope: encodeURIComponent(scopes),
+        redirect_uri: encodeURIComponent(redirectUri)
+    })
+    res.redirect(`https://accounts.spotify.com/authorize?${query}`)
 })
 
 app.get('/ok', (req, res) => {
     const authorization = Buffer.from(
-        `${config.clientId}:${config.clientSecret}`
+        `${config.clientId}:${config.clientSecret}`,
     ).toString('base64')
     request.post(
         {
@@ -52,18 +54,18 @@ app.get('/ok', (req, res) => {
             form: {
                 code: req.query.code,
                 grant_type: 'authorization_code',
-                redirect_uri: config.redirectUri
+                redirect_uri: config.redirectUri,
             },
             headers: {
-                Authorization: `Basic ${authorization}`
-            }
+                Authorization: `Basic ${authorization}`,
+            },
         },
         (error, response, body) => {
             const data = JSON.parse(body)
             req.spotishare.access_token = data.access_token
             req.spotishare.refresh_token = data.refresh_token
             res.redirect(config.frontUri)
-        }
+        },
     )
 })
 app.use(middlewares.authentication)
